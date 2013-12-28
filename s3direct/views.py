de0d731e526be14524b24a3a80adcdb24c179c16
Miguel Pachas García <miguel.pachas.garcia@gmail.com>
2013-12-28 13:35:18 +0100

    replace false

--- s3direct/views.py
+++ s3direct/views.py
@@ -42,6 +42,7 @@ def get_upload_params(request, upload_to=''):
         "form_action": "https://%s.s3.amazonaws.com" % settings.AWS_STORAGE_BUCKET_NAME,
         "success_action_status": "201",
         "acl": "public-read",
+        "replace": False,
         "Content-Type": content_type
     }
 
