2fea94735ec2262e78f518d82bdc551db50c94a4
Miguel Pachas García <miguel.pachas.garcia@gmail.com>
2014-03-21 16:58:11 +0100

    hola

--- s3direct/views.py
+++ s3direct/views.py
@@ -1,4 +1,4 @@
-import sha, uuid, hmac, json
+import hashlib, uuid, hmac, json
 from datetime import datetime, timedelta
 from base64 import b64encode
 from django.conf import settings
@@ -9,40 +9,58 @@ from django.views.decorators.http import require_POST
 
 
 S3DIRECT_DIR = getattr(settings, "S3DIRECT_DIR", 's3direct')
+S3DIRECT_UNIQUE_RENAME = getattr(settings, "S3DIRECT_UNIQUE_RENAME", None)
 
 
 @csrf_exempt
 @require_POST
 @user_passes_test(lambda u: u.is_staff)
 def get_upload_params(request, upload_to=''):
-    
     content_type = request.POST['type']
-    expires = (datetime.now() + timedelta(hours=24)).strftime('%Y-%m-%dT%H:%M:%S.000Z')
-    
+    source_filename = request.POST['name']
+    data = create_upload_data(content_type, source_filename, upload_to)
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+def create_upload_data(content_type, source_filename, upload_to):
+    access_key = settings.AWS_ACCESS_KEY_ID
+    secret_access_key = settings.AWS_SECRET_ACCESS_KEY
+    bucket = settings.AWS_STORAGE_BUCKET_NAME
+
+    expires_in = datetime.now() + timedelta(hours=24)
+    expires = expires_in.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+
     policy_object = json.dumps({
         "expiration": expires,
         "conditions": [
-            {"bucket": settings.AWS_STORAGE_BUCKET_NAME},
+            {"bucket": bucket},
             {"acl": "public-read"},
             {"Content-Type": content_type},
             ["starts-with", "$key", ""],
             {"success_action_status": "201"}
         ]
     })
-    
+
     policy = b64encode(policy_object.replace('\n', '').replace('\r', ''))
-    signature = b64encode(hmac.new(settings.AWS_SECRET_ACCESS_KEY, policy, sha).digest())
-    key = "%s/%s/${filename}" % (upload_to or S3DIRECT_DIR, uuid.uuid4().hex)
-    
-    data = {
+    signature = hmac.new(secret_access_key, policy, hashlib.sha1).digest()
+    signature_b64 = b64encode(signature)
+
+    if S3DIRECT_UNIQUE_RENAME:
+        ext = source_filename.split('.')[-1]
+        filename = '%s.%s' % (uuid.uuid4(), ext)
+    else:
+        filename = '${filename}'
+
+    key = "%s/%s" % (upload_to or S3DIRECT_DIR, filename)
+    bucket_url = 'https://s3.amazonaws.com/%s' % bucket
+
+    return {
         "policy": policy,
-        "signature": signature,
+        "signature": signature_b64,
         "key": key,
-        "AWSAccessKeyId": settings.AWS_ACCESS_KEY_ID,
-        "form_action": "https://%s.s3.amazonaws.com" % settings.AWS_STORAGE_BUCKET_NAME,
+        "AWSAccessKeyId": access_key,
+        "form_action": bucket_url,
         "success_action_status": "201",
         "acl": "public-read",
         "Content-Type": content_type
     }
-
-    return HttpResponse(json.dumps(data), content_type="application/json")
