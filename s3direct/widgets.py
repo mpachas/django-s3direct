f6201b689c91f3ce34b675460e63add21e1e0391
Miguel Pachas García <miguel.pachas.garcia@gmail.com>
2014-03-22 02:03:21 +0100

    widgets order

--- s3direct/widgets.py
+++ s3direct/widgets.py
@@ -27,8 +27,8 @@ class S3DirectEditor(widgets.TextInput):
     class Media:
         js = (
             's3direct/js/jquery-1.10.2.min.js',
-            's3direct/js/jquery.iframe-transport.js',
             's3direct/js/jquery.ui.widget.js',
+            's3direct/js/jquery.iframe-transport.js',
             's3direct/js/jquery.fileupload.js',
             's3direct/js/s3direct.js',
         )
@@ -66,8 +66,8 @@ class S3DirectFileUpload(widgets.TextInput):
     class Media:
         js = (
             's3direct/js/jquery-1.10.2.min.js',
-            's3direct/js/jquery.iframe-transport.js',
             's3direct/js/jquery.ui.widget.js',
+            's3direct/js/jquery.iframe-transport.js',
             's3direct/js/jquery.fileupload.js',
             's3direct/js/s3direct.js',
         )
